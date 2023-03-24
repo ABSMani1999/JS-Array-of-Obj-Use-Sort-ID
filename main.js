2339fbaa6455ebbe2902011de8476cc440b9557f
Balasubramanian1999 <absmani1999@gmail.com>
2023-03-24 19:29:25 +0530

    new file add

---
+++ main.js
@@ -0,0 +1,51 @@
+
+//8. Sort an array of JavaScript objects based on libraryID
+var library = [
+
+	{
+	title: 'The Road Ahead',
+
+	author: 'Bill Gates',
+
+	libraryID: 1254
+	},
+
+	{
+	title: 'Walter Isaacson',
+
+	author: 'Steve Jobs',
+
+	libraryID: 4264
+	},
+
+	{
+	title: 'Mockingjay: The Final Book of The Hunger Games',
+
+	author: 'Suzanne Collins',
+
+	libraryID: 3245
+	},
+
+	{
+	title: 'Adventures of Tom Sawyer',
+
+	author: 'Mark Twain',
+
+	libraryID: 1345
+	},
+
+	{
+	title: 'Adventures of Sherlock Holmes',
+
+	author: 'Sir Arthur Conan Doyle',
+
+	libraryID: 2245
+	}
+
+];
+
+var a=library.sort(function(a,b){
+	return a.libraryID-b.libraryID;
+})
+console.log(a);
+
